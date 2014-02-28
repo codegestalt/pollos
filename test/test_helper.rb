9c62bcc98ce86f0cb93d8217db5eb2b20479a3ec
Rodrigo Haenggi <rodrigo@codegestalt.com>
2014-02-28 18:10:16 +0100

    remove unused endpoint

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,8 +2,4 @@ $LOAD_PATH.unshift(File.expand_path("../../lib", __FILE__))
 
 require 'pollos'
 require 'minitest/autorun'
-
-class MiniTest::Test
-  GET_ENDPOINT = "http://status.codegestalt.com/api/v1/apps"
-  POST_ENDPOINT = "http://status.codegestalt.com/api/v1/messages"
-end
+require 'mocha'

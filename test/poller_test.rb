c684244049517816f02b8d28958f3dba04a915ec
Rodrigo Haenggi <rodrigo@codegestalt.com>
2014-03-04 01:17:46 +0100

    rename pollos_test to poller_test

--- test/poller_test.rb
+++ test/poller_test.rb
@@ -1,7 +1,7 @@
 require File.expand_path("../test_helper", __FILE__)
 
 module Pollos
-  class PollosTest < MiniTest::Test
+  class PollerTest < MiniTest::Test
 
     def setup
       @poller = Poller.new(5, "http://codegestalt.com", "http://codegestalt.com")
